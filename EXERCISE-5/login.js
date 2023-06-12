094356bd15c22d04f90929d2f529b8fe5822f067
pavan <67680131+pavan2580-dmpt@users.noreply.github.com>
2023-06-12 14:58:34 +0530

    exercise-5

---
+++ EXERCISE-5/login.js
@@ -0,0 +1,13 @@
+class User{
+    Constructor(name,pass)
+    {
+    this.username = name;
+    this.password = pass;
+    }
+    validateUser(name,pass)
+    {
+    return name = (this.name && pass==this.password) ? "Login Successful" : "Unauthorized access";
+    }
+    }
+    
+export default User
